bf401d183a6695c850c15bc3d8d563531c7f2263
Devarshi Bhojak <59047224+Dev-Bhojak@users.noreply.github.com>
2020-08-18 00:58:54 +1000

    Update colorGame.js
    Minor edits

--- colorGame.js
+++ colorGame.js
@@ -25,20 +25,11 @@ function setupModeButton(){
             modeButtons[0].classList.remove("selected");
             modeButtons[1].classList.remove("selected");
             this.classList.add("selected");
-    
-            // if (this.textContent ==="Easy"){
-            //     numSquares = 3
-            // }else{  
-            //     numSquares = 6
-            // }
+
             this.textContent === "Easy" ? numSquares= 3: numSquares= "6";
     
             reset();
-            // figure out how many squares to show
-            // pick new colors
-            // pick a new picked color
-            // update page to refelect changes
-    
+
         });
     }
 }
@@ -86,38 +77,6 @@ function reset(){
       }
       h1.style.backgroundColor ="steelblue";
 }
-
-// easyBtn.addEventListener("click", function(){
-//     hardBtn.classList.remove("selected");
-//     easyBtn.classList.add("selected");
-//     numSquares =3;
-//     colors = generateRandomColor(numSquares);
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-
-//     for( var i =0; i < squares.length; i++){
-//         if(colors[i]){
-//             squares[i].style.backgroundColor = colors[i];
-//         }else{ 
-//             squares[i].style.display = "none";}   
-//     }
-//     }
-// );
-
-// hardBtn.addEventListener("click", function(){
-//     easyBtn.classList.remove("selected");
-//     hardBtn.classList.add("selected");
-//     numSquares = 6;
-//     colors = generateRandomColor(numSquares);
-//     pickedColor = pickColor();
-//     colorDisplay.textContent = pickedColor;
-
-//     for( var i =0; i < squares.length; i++){
-//     squares[i].style.backgroundColor = colors[i];
-//     squares[i].style.display = "block";
-//     }
-// })
-
 
 resetButton.addEventListener("click", function(){
    
@@ -170,21 +129,5 @@ function randomColor(){
     
     return "rgb(" + r + ", " + g + ", " + b + ")"
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 },500);
 
